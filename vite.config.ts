25613fe6587c4809316b9609e6cbb6abb00e1d26
Sawai <sawai.reo@gmail.com>
2025-08-15 18:41:08 +0900

    [update] CD用に調整
    いずれ修正する

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/data-flow-creator/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
